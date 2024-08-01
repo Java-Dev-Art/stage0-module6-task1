203f23a92b2fe09239f2f54312fef5f1ccfb9312
Artur Markouski <markouski.artur@student.ehu.lt>
2024-08-01 22:35:11 +0200

    feat: add solv all tasks

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,31 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean  hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public String getDescription(){
+        String paw = (getNumberOfPaws() > 1) ? " paws " : " paw ";
+        String fur = (isHasFur()) ? " a " : " no ";
+        return "This animal is mostly " + getColor() +". It has " + getNumberOfPaws() + paw + "and" + fur + "fur.";
+    }
 }
